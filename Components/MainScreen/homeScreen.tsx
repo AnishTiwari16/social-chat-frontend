bef20c644de1dc8c5dda0f75e8a3ccc5e6485bb9
Anish Tiwari <anish.at.tiwari@gmail.com>
2023-05-28 20:37:14 +0530

    feat: added chatsScreen

--- Components/MainScreen/homeScreen.tsx
+++ Components/MainScreen/homeScreen.tsx
@@ -1,19 +1,19 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import Navbar from './navbar';
-import Cards from './cards';
-import { useTailwind } from 'tailwind-rn/dist';
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Navbar from "./navbar";
+import Cards from "./cards";
+import { useTailwind } from "tailwind-rn/dist";
 
-const HomeScreen = () => {
-    const tw = useTailwind();
-    return (
-        <SafeAreaView>
-            <View style={tw('p-2.5 bg-white')}>
-                <Navbar />
-                <Cards />
-            </View>
-        </SafeAreaView>
-    );
+const HomeScreen = ({ navigation }) => {
+  const tw = useTailwind();
+  return (
+    <SafeAreaView>
+      <View style={tw("p-2.5 bg-white")}>
+        <Navbar navigation={navigation} />
+        <Cards />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
--- Components/MainScreen/navbar.tsx
+++ Components/MainScreen/navbar.tsx
@@ -1,23 +1,25 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { useTailwind } from 'tailwind-rn/dist';
-const Navbar = () => {
-    const tw = useTailwind();
-    return (
-        <View style={styles.navbarContainer}>
-            <Text style={tw('text-xl text-blue-500')}>kite</Text>
-            <Image
-                source={require('../../assets/PostIcons/chatIcon.png')}
-                style={tw('ml-auto')}
-            />
-        </View>
-    );
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { useTailwind } from "tailwind-rn/dist";
+const Navbar = ({ navigation }) => {
+  const tw = useTailwind();
+  return (
+    <View style={styles.navbarContainer}>
+      <Text style={tw("text-xl text-blue-500")}>kite</Text>
+      <TouchableOpacity
+        style={tw("ml-auto")}
+        onPress={() => navigation.navigate("ChatsScreen")}
+      >
+        <Image source={require("../../assets/PostIcons/chatIcon.png")} />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default Navbar;
 
 const styles = StyleSheet.create({
-    navbarContainer: {
-        flexDirection: 'row',
-    },
+  navbarContainer: {
+    flexDirection: "row",
+  },
 });
