e2b405ecb17a22c0931bb0fb354e50a7f1c2a813
Anish Tiwari <anish.at.tiwari@gmail.com>
2023-06-11 23:50:14 +0530

    feat: added api integration and validation

--- Screens/onBoarding/signUp/signUpAccountCreated.tsx
+++ Screens/onBoarding/signUp/signUpAccountCreated.tsx
@@ -1,55 +1,74 @@
 import {
-    Button,
-    Keyboard,
-    KeyboardAvoidingView,
-    Platform,
-    SafeAreaView,
-    Text,
-    TextInput,
-    TouchableWithoutFeedback,
-    View,
-} from 'react-native';
-import React from 'react';
-import { useTailwind } from 'tailwind-rn';
-import Select from 'react-native-vector-icons/AntDesign';
-const SignUpAccountCreated = ({ navigation }) => {
-    const tw = useTailwind();
-    const [userName, setUserName] = React.useState('');
-    return (
-        <SafeAreaView>
-            <KeyboardAvoidingView
-                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            >
-                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                    <View style={tw('px-10 py-28')}>
-                        <Text style={tw(' font-medium text-2xl text-gray-700')}>
-                            Let's Roll animation
-                        </Text>
-                        <View style={tw('py-12')}>
-                            <Select
-                                name="edit"
-                                size={50}
-                                color="#9b9b9b"
-                                style={tw('text-center pb-5')}
-                            />
-                            <Text style={tw('text-center text-blue-500 pb-5')}>
-                                Account created successfully
-                            </Text>
-                            <View style={tw('bg-blue-500 p-1 rounded')}>
-                                <Button
-                                    title="Let's Roll"
-                                    color="white"
-                                    // onPress={() =>
-                                    //     navigation.navigate('SignUpPassword')
-                                    // }
-                                />
-                            </View>
-                        </View>
-                    </View>
-                </TouchableWithoutFeedback>
-            </KeyboardAvoidingView>
-        </SafeAreaView>
-    );
+  Alert,
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import React from "react";
+import { useTailwind } from "tailwind-rn";
+import Select from "react-native-vector-icons/AntDesign";
+const SignUpAccountCreated = ({ navigation, route }) => {
+  const { email, username, password } = route.params;
+  const tw = useTailwind();
+  const [userName, setUserName] = React.useState("");
+
+  const handleSignin = async () => {
+    //hit the signup api and store the user in db
+    try {
+      const resp = await fetch("http://localhost:8000/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, username, password }),
+      });
+      const data = resp.json();
+      data.then((res) => {
+        Alert.alert(res.message);
+        navigation.navigate("LogIn");
+      });
+    } catch (err) {
+      Alert.alert("Something went wrong, please try again later");
+    }
+  };
+  return (
+    <SafeAreaView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={tw("px-10 py-28")}>
+            <Text style={tw(" font-medium text-2xl text-gray-700")}>
+              Let's Roll animation
+            </Text>
+            <View style={tw("py-12")}>
+              <Select
+                name="edit"
+                size={50}
+                color="#9b9b9b"
+                style={tw("text-center pb-5")}
+              />
+              <Text style={tw("text-center text-blue-500 pb-5")}>
+                Account created successfully
+              </Text>
+              <View style={tw("bg-blue-500 p-1 rounded")}>
+                <Button
+                  title="Let's Roll"
+                  color="white"
+                  onPress={() => handleSignin()}
+                />
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 };
 
 export default SignUpAccountCreated;
--- Screens/onBoarding/signUp/signUpUsername.tsx
+++ Screens/onBoarding/signUp/signUpUsername.tsx
@@ -1,64 +1,96 @@
 import {
-    Button,
-    Keyboard,
-    KeyboardAvoidingView,
-    Platform,
-    SafeAreaView,
-    Text,
-    TextInput,
-    TouchableWithoutFeedback,
-    View,
-} from 'react-native';
-import React from 'react';
-import { useTailwind } from 'tailwind-rn';
-import Select from 'react-native-vector-icons/AntDesign';
-const SignUpUsername = ({ navigation }) => {
-    const tw = useTailwind();
-    const [userName, setUserName] = React.useState('');
-    return (
-        <SafeAreaView>
-            <KeyboardAvoidingView
-                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            >
-                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                    <View style={tw('px-10 py-28')}>
-                        <Text style={tw(' font-medium text-2xl text-gray-700')}>
-                            Choose a username
-                        </Text>
-                        <View style={tw('py-12')}>
-                            <Select
-                                name="edit"
-                                size={50}
-                                color="#9b9b9b"
-                                style={tw('text-center pb-5')}
-                            />
-                            <Text style={tw('text-center text-blue-500')}>
-                                Choose a Username
-                            </Text>
+  Alert,
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import React from "react";
+import { useTailwind } from "tailwind-rn";
+import Select from "react-native-vector-icons/AntDesign";
+const SignUpUsername = ({ navigation, route }) => {
+  const { email } = route.params;
+  const tw = useTailwind();
+  const [userName, setUserName] = React.useState("");
+  const [usernameStatus, setUsernameStatusStatus] = React.useState("");
 
-                            <TextInput
-                                style={tw(
-                                    'h-12 border-2 border-neutral-300 p-2.5 my-5'
-                                )}
-                                placeholder="Enter a username"
-                                onChangeText={(e) => setUserName(e)}
-                                value={userName}
-                            />
-                            <View style={tw('bg-blue-500 p-1 rounded')}>
-                                <Button
-                                    title="Next"
-                                    color="white"
-                                    onPress={() =>
-                                        navigation.navigate('SignUpPassword')
-                                    }
-                                />
-                            </View>
-                        </View>
-                    </View>
-                </TouchableWithoutFeedback>
-            </KeyboardAvoidingView>
-        </SafeAreaView>
-    );
+  const handleUsername = async () => {
+    try {
+      const resp = await fetch("http://localhost:8000/checkusername", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: email, username: userName }),
+      });
+      const data = resp.json();
+      data.then((res) => {
+        if (res.error) {
+          setUsernameStatusStatus(res.message);
+        } else {
+          navigation.navigate("SignUpPassword", {
+            email: res.email,
+            username: res.username,
+          });
+        }
+      });
+    } catch (err) {
+      Alert.alert("Something went wrong, please try again later");
+    }
+  };
+  React.useEffect(() => {
+    if (usernameStatus) {
+      setTimeout(() => {
+        setUsernameStatusStatus("");
+      }, 3000);
+    }
+  }, [usernameStatus]);
+  return (
+    <SafeAreaView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={tw("px-10 py-28")}>
+            <Text style={tw(" font-medium text-2xl text-gray-700")}>
+              Choose a username
+            </Text>
+            <View style={tw("py-12")}>
+              <Select
+                name="edit"
+                size={50}
+                color="#9b9b9b"
+                style={tw("text-center pb-5")}
+              />
+              <Text style={tw("text-center text-blue-500")}>
+                Choose a Username
+              </Text>
+              <View style={tw("mt-5 mb-3")}>
+                <TextInput
+                  style={tw("h-12 border-2 border-neutral-300 p-2.5 my-5")}
+                  placeholder="Enter a username"
+                  onChangeText={(e) => setUserName(e)}
+                />
+                <Text style={tw("text-red-500 text-sm")}>{usernameStatus}</Text>
+              </View>
+              <View style={tw("bg-blue-500 p-1 rounded")}>
+                <Button
+                  title="Next"
+                  color="white"
+                  onPress={() => handleUsername()}
+                />
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 };
 
 export default SignUpUsername;
